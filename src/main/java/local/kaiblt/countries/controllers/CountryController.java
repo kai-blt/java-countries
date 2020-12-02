e76720b7e466095eafa5fb829fe7ce46096a602e
kai-blt <blaitong@gmail.com>
2020-12-02 08:35:37 -0800

    added get total population endpoint

--- src/main/java/local/kaiblt/countries/controllers/CountryController.java
+++ src/main/java/local/kaiblt/countries/controllers/CountryController.java
@@ -14,14 +14,21 @@ import java.util.List;
 
 @RestController
 public class CountryController {
+    private List<Country> countryList = new ArrayList<>();
+
+    private void refreshList() {
+        countryList.clear();
+        countryrepos.findAll().iterator().forEachRemaining(countryList::add);
+    }
+
     //connect controller to repository
     @Autowired
     CountryRepository countryrepos;
 
+
     private List<Country> filterCountries(List<Country> list, CheckCountries test) {
         //Create temporary list
         List<Country> filteredList = new ArrayList<>();
-
         //Check if filter criteria met. If so add to list.
         for (Country c : list) {
             if (test.filter(c)) {
@@ -34,8 +41,7 @@ public class CountryController {
     // http://localhost:2019/names/all
     @GetMapping(value = "/names/all", produces = "application/json")
     public ResponseEntity<?> getAllCountries() {
-        List<Country> countryList = new ArrayList<>();
-        countryrepos.findAll().iterator().forEachRemaining(countryList::add);
+        refreshList();
         return new ResponseEntity<>(countryList, HttpStatus.OK);
     }
 
@@ -44,15 +50,27 @@ public class CountryController {
     // typed this way in the url
     @GetMapping(value = "/names/start/{letter}", produces = "application/json")
     public ResponseEntity<?> getByLetter(@PathVariable char letter) {
-        List<Country> countryList = new ArrayList<>();
-        countryrepos.findAll().iterator().forEachRemaining(countryList::add);
+        refreshList();
         List<Country> filteredList = filterCountries(countryList, c -> c.getName().charAt(0) == Character.toUpperCase(letter));
+        filteredList.sort((c1, c2) -> c1.getName().compareToIgnoreCase(c2.getName()));
         return new ResponseEntity<>(filteredList, HttpStatus.OK);
     }
 
     // http://localhost:2019/population/total
+    @GetMapping(value = "/population/total", produces = "application/json")
+    public ResponseEntity<?> getTotalPopulation() {
+        refreshList();
+        long totalPopulation = 0;
+        for (Country c : countryList) {
+            totalPopulation += c.getPopulation();
+        }
+        System.out.println("The Total Population is " + totalPopulation);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 
     // http://localhost:2019/population/min
+
 
     // http://localhost:2019/population/max
 
