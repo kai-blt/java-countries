51789a5300032706007d972a39c034420727f52b
kai-blt <blaitong@gmail.com>
2020-12-02 08:15:24 -0800

    added search by first letter endpoint and filter interface

--- src/main/java/local/kaiblt/countries/controllers/CountryController.java
+++ src/main/java/local/kaiblt/countries/controllers/CountryController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -17,6 +18,18 @@ public class CountryController {
     @Autowired
     CountryRepository countryrepos;
 
+    private List<Country> filterCountries(List<Country> list, CheckCountries test) {
+        //Create temporary list
+        List<Country> filteredList = new ArrayList<>();
+
+        //Check if filter criteria met. If so add to list.
+        for (Country c : list) {
+            if (test.filter(c)) {
+                filteredList.add(c);
+            }
+        }
+        return filteredList;
+    }
 
     // http://localhost:2019/names/all
     @GetMapping(value = "/names/all", produces = "application/json")
@@ -27,6 +40,15 @@ public class CountryController {
     }
 
     // http://localhost:2019/names/start/u
+    // Note lowercase "u" in url. Character.toUpperCase is used on the PathVariable to match uppercase country names when
+    // typed this way in the url
+    @GetMapping(value = "/names/start/{letter}", produces = "application/json")
+    public ResponseEntity<?> getByLetter(@PathVariable char letter) {
+        List<Country> countryList = new ArrayList<>();
+        countryrepos.findAll().iterator().forEachRemaining(countryList::add);
+        List<Country> filteredList = filterCountries(countryList, c -> c.getName().charAt(0) == Character.toUpperCase(letter));
+        return new ResponseEntity<>(filteredList, HttpStatus.OK);
+    }
 
     // http://localhost:2019/population/total
 
